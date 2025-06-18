1cc0fba108724cc22b01e69a998baf6fb4117111
simon.vonroenn <simon.vonroenn@web.de>
2025-06-18 12:00:39 +0200

    Create first basic structure for RepoGenerator.xtend

--- org.xtext.componentsystem.dsl/src/codegen/RepoGenerator.xtend
+++ org.xtext.componentsystem.dsl/src/codegen/RepoGenerator.xtend
@@ -4,27 +4,53 @@ import org.eclipse.xtext.generator.IGenerator
 import org.eclipse.emf.ecore.resource.Resource
 import org.eclipse.xtext.generator.IFileSystemAccess
 import org.eclipse.emf.ecore.EObject
-import MDSDComponentMetamodel.ComponentSystem
 import MDSDComponentMetamodel.SystemIndependant.Repository
-import MDSDComponentMetamodel.SystemIndependant.Component
+import MDSDComponentMetamodel.SystemIndependant.Interface
+import MDSDComponentMetamodel.SystemIndependant.BasicComponent
 
 class RepoGenerator implements IGenerator {
-	
-	override doGenerate(Resource resource, IFileSystemAccess fsa) {
-		for (EObject o : resource.contents) {
-            o.compile(fsa)
+    
+    final String JAVA_SUFFIX = ".java";
+    
+    override void doGenerate(Resource resource, IFileSystemAccess fsa) {
+        for(repository: resource.allContents.toIterable.filter(Repository)) {
+            fsa.generateFile(getPackage(repository).replace(".", "/") + "/" +  "Helper" + JAVA_SUFFIX, repository.compile)    
         }
-	}
-	
-	def dispatch void compile(ComponentSystem cs, IFileSystemAccess fsa) {
-		for (Repository repo : cs.getRepositories()) {
-			for (Component comp : repo.getComponents()) {
-				fsa.generateFile(comp.name + ".txt", '''
-        		this is component «comp.name»
-        		''')
-			}
-		}
-	}
-	
-	def dispatch void compile(EObject object, IFileSystemAccess access) {}
+        for(interface: resource.allContents.toIterable.filter(Interface)) {
+            fsa.generateFile(getPackage(interface).replace(".", "/") + "/" +  getInterfaceName(interface) + JAVA_SUFFIX, interface.compile)    
+        }
+        for(component: resource.allContents.toIterable.filter(BasicComponent)) {
+            fsa.generateFile(getPackage(component).replace(".", "/") + "/" +  getComponentName(component) + JAVA_SUFFIX, component.compile)    
+        }
+    }
+    
+    def compile(Repository repo) '''
+    '''
+    
+    def compile(Interface interf) '''
+    '''
+    
+    def compile(BasicComponent comp) '''
+    // this is component «comp.name».
+    '''
+    
+    def String getPackage(EObject object) {
+        if(object instanceof Repository) {
+            return "repository";
+        } else if (object instanceof Interface) {
+            return "interface";
+        } else if (object instanceof BasicComponent) {
+            return object.name;
+        }
+        return "unknownObject";
+    }
+    
+    
+    def String getInterfaceName(Interface interf) {
+        return "I" + interf.name;
+    }
+    
+    def String getComponentName(BasicComponent comp) {
+        return comp.name + "Impl";
+    }
 }
--- org.xtext.componentsystem.dsl/xtend-gen/codegen/RepoGenerator.java
+++ org.xtext.componentsystem.dsl/xtend-gen/codegen/RepoGenerator.java
@@ -1,59 +1,92 @@
 package codegen;
 
-import MDSDComponentMetamodel.ComponentSystem;
 import MDSDComponentMetamodel.SystemIndependant.BasicComponent;
-import MDSDComponentMetamodel.SystemIndependant.Component;
+import MDSDComponentMetamodel.SystemIndependant.Interface;
 import MDSDComponentMetamodel.SystemIndependant.Repository;
-import java.util.Arrays;
-import org.eclipse.emf.common.util.EList;
+import com.google.common.collect.Iterables;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtend2.lib.StringConcatenation;
 import org.eclipse.xtext.generator.IFileSystemAccess;
 import org.eclipse.xtext.generator.IGenerator;
-import org.eclipse.xtext.xbase.lib.XbaseGenerated;
+import org.eclipse.xtext.xbase.lib.IteratorExtensions;
 
 @SuppressWarnings("all")
 public class RepoGenerator implements IGenerator {
+  private final String JAVA_SUFFIX = ".java";
+
   @Override
   public void doGenerate(final Resource resource, final IFileSystemAccess fsa) {
-    EList<EObject> _contents = resource.getContents();
-    for (final EObject o : _contents) {
-      this.compile(o, fsa);
+    Iterable<Repository> _filter = Iterables.<Repository>filter(IteratorExtensions.<EObject>toIterable(resource.getAllContents()), Repository.class);
+    for (final Repository repository : _filter) {
+      String _replace = this.getPackage(repository).replace(".", "/");
+      String _plus = (_replace + "/");
+      String _plus_1 = (_plus + "Helper");
+      String _plus_2 = (_plus_1 + this.JAVA_SUFFIX);
+      fsa.generateFile(_plus_2, this.compile(repository));
+    }
+    Iterable<Interface> _filter_1 = Iterables.<Interface>filter(IteratorExtensions.<EObject>toIterable(resource.getAllContents()), Interface.class);
+    for (final Interface interface_ : _filter_1) {
+      String _replace_1 = this.getPackage(interface_).replace(".", "/");
+      String _plus_3 = (_replace_1 + "/");
+      String _interfaceName = this.getInterfaceName(interface_);
+      String _plus_4 = (_plus_3 + _interfaceName);
+      String _plus_5 = (_plus_4 + this.JAVA_SUFFIX);
+      fsa.generateFile(_plus_5, this.compile(interface_));
+    }
+    Iterable<BasicComponent> _filter_2 = Iterables.<BasicComponent>filter(IteratorExtensions.<EObject>toIterable(resource.getAllContents()), BasicComponent.class);
+    for (final BasicComponent component : _filter_2) {
+      String _replace_2 = this.getPackage(component).replace(".", "/");
+      String _plus_6 = (_replace_2 + "/");
+      String _componentName = this.getComponentName(component);
+      String _plus_7 = (_plus_6 + _componentName);
+      String _plus_8 = (_plus_7 + this.JAVA_SUFFIX);
+      fsa.generateFile(_plus_8, this.compile(component));
     }
   }
 
-  protected void _compile(final ComponentSystem cs, final IFileSystemAccess fsa) {
-    EList<Repository> _repositories = cs.getRepositories();
-    for (final Repository repo : _repositories) {
-      EList<BasicComponent> _components = repo.getComponents();
-      for (final Component comp : _components) {
-        String _name = comp.getName();
-        String _plus = (_name + ".txt");
-        StringConcatenation _builder = new StringConcatenation();
-        _builder.append("this is component ");
-        String _name_1 = comp.getName();
-        _builder.append(_name_1);
-        _builder.newLineIfNotEmpty();
-        fsa.generateFile(_plus, _builder);
+  public CharSequence compile(final Repository repo) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder;
+  }
+
+  public CharSequence compile(final Interface interf) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder;
+  }
+
+  public CharSequence compile(final BasicComponent comp) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("// this is component ");
+    String _name = comp.getName();
+    _builder.append(_name);
+    _builder.append(".");
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+
+  public String getPackage(final EObject object) {
+    if ((object instanceof Repository)) {
+      return "repository";
+    } else {
+      if ((object instanceof Interface)) {
+        return "interface";
+      } else {
+        if ((object instanceof BasicComponent)) {
+          return ((BasicComponent)object).getName();
+        }
       }
     }
+    return "unknownObject";
   }
 
-  protected void _compile(final EObject object, final IFileSystemAccess access) {
+  public String getInterfaceName(final Interface interf) {
+    String _name = interf.getName();
+    return ("I" + _name);
   }
 
-  @XbaseGenerated
-  public void compile(final EObject cs, final IFileSystemAccess fsa) {
-    if (cs instanceof ComponentSystem) {
-      _compile((ComponentSystem)cs, fsa);
-      return;
-    } else if (cs != null) {
-      _compile(cs, fsa);
-      return;
-    } else {
-      throw new IllegalArgumentException("Unhandled parameter types: " +
-        Arrays.<Object>asList(cs, fsa).toString());
-    }
+  public String getComponentName(final BasicComponent comp) {
+    String _name = comp.getName();
+    return (_name + "Impl");
   }
 }
